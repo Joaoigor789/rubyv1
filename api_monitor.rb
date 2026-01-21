187c29189fc92ad9f377d6e1dd9dab1091c657b0
Joao igor <joaoigor840@gmail.com>
2026-01-21 18:22:36 -0300

    update

--- api_monitor.rb
+++ api_monitor.rb
@@ -1,14 +1,21 @@
 require 'httparty'
 require 'json'
+require 'date'
 
-def buscar_asteroides_simples
-    url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-01-20&end_date=2024-01-21&api_key=DEMO_KEY"
+def buscar_asteroides_tempo_real
+    hoje = Date.today.strftime("%Y-%m-%d")
+    amanha = (Date.today + 1).strftime("%Y-%m-%d")
+
+
+    url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=#{hoje}&end_date=#{amanha}&api_key=DEMO_KEY"
 
     respostas = HTTParty.get(url)
     dados = JSON.parse(respostas.body)
 
     puts "Monitoramento Universal"
-    puts "=" * 40
+    puts "=" * 50
+    puts "Tempo real: #{hoje} a #{amanha}"
+    puts "=" * 50
 
     if dados["near_earth_objects"]
         dados["near_earth_objects"].each do |data, asteroides|
@@ -23,14 +30,20 @@ def buscar_asteroides_simples
     if a["close_approach_data"] && a["close_approach_data"][0]
                 
                 velocidade = a["close_approach_data"][0]["relative_velocity"]["kilometers_per_hour"].to_f.round(2)
-                puts "- #{nome}: Perigoso? #{perigoso} | Velocidade: #{velocidade} km/h"
+                distancia = a["close_approach_data"][0]["miss_distance"]["kilometers"].to_f.round(2)
+
+                puts " #{nome}"
+                puts " Perigoso? #{perigoso}"
+                puts " Velocidade? #{velocidade} km/h"
+                puts " Distância? #{distancia} km"
+                puts " =================="
+    
     else
-                puts "- #{nome}: Perigoso? #{perigoso} | Velocidade: N/A"
+                puts "- #{nome}: Perigoso? #{perigoso} | Dados não disponiveis"
     end
             end
         end
     end
 end
 
-buscar_asteroides_simples
-
+buscar_asteroides_tempo_real
