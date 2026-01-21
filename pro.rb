6e684928f2c526bdc46e78fb4718fe0e7fcf9206
Joao igor <joaoigor1103@gmail.com>
2026-01-21 19:11:38 -0300

    update

--- pro.rb
+++
@@ -1,7 +0,0 @@
-lista = ["Ler o Livro", "Comer Bolo"]
-feitas = [true, false] 
-puts "Eu fiz a tarefa"
-lista.each_with_index do |tarefa, i|
-    status = feitas[i] ? "[aceito]" : "[negado]"
-    puts "#{status} #{tarefa}"
-end
